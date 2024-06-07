4eb12ab0c9628b2156fb9346ed789009afc31995
anaximeno <anaximenobrito@gmail.com>
2024-06-07 05:15:11 -0100

    chore: initialize java project

---
+++ src/main/java/asa/fordfulkerson/App.java
@@ -0,0 +1,13 @@
+package asa.fordfulkerson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
